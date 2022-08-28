c3531ced2ad6c2a4d36dcf3a6f078d1eb6b91ef7
Jason R. McNeil <jason@mcneil.dev>
2022-08-28 10:19:14 -0700

    Default ApplicationResourcesBaseUri to empty string

--- Meteia/Application/DependencyInjection.php
+++ Meteia/Application/DependencyInjection.php
@@ -5,9 +5,12 @@ declare(strict_types=1);
 use Meteia\Application\ApplicationResourcesBaseUri;
 use Meteia\Application\ApplicationPublicDir;
 use Meteia\Application\ApplicationResources;
+use Meteia\Configuration\Configuration;
 
 return [
-    ApplicationResourcesBaseUri::class => fn () => new ApplicationResourcesBaseUri('http://127.0.0.1:5173'),
+    ApplicationResourcesBaseUri::class => function (Configuration $configuration) {
+        return new ApplicationResourcesBaseUri($configuration->string('RESOURCES_BASE_URI', ''));
+    },
     ApplicationResources::class => function (ApplicationResourcesBaseUri $applicationResourcesBaseUri, ApplicationPublicDir $publicDir): ApplicationResources {
         return new ApplicationResources($applicationResourcesBaseUri, $publicDir, $publicDir->join('dist/manifest.json'));
     },
