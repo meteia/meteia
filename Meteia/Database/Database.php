d37ba3bbc8b7f6650c43f575575a32c25a7646b1
Jason R. McNeil <jason@mcneil.dev>
2022-09-04 21:43:33 -0700

    database: add prepareBindings()

--- Meteia/Database/Database.php
+++ Meteia/Database/Database.php
@@ -5,7 +5,37 @@ declare(strict_types=1);
 namespace Meteia\Database;
 
 use Aura\Sql\ExtendedPdo;
+use Meteia\ValueObjects\Identity\UniqueId;
 
 class Database extends ExtendedPdo
 {
+
+    public function prepareBindings(array $values): array
+    {
+        return array_map($this->prepareBoundValue(...), $values);
+    }
+
+    private function prepareBoundValue(mixed $value): mixed
+    {
+        if (is_array($value)) {
+            return array_map($this->prepareBoundValue(...), $value);
+        }
+        if (!is_object($value)) {
+            return $value;
+        }
+        if ($value instanceof UniqueId) {
+            return $value->bytes;
+        }
+        if ($value instanceof \StringBackedEnum) {
+            return $value->value;
+        }
+        if ($value instanceof \IntBackedEnum) {
+            return $value->value;
+        }
+        if ($value instanceof \Stringable) {
+            return (string) $value;
+        }
+
+        return $value;
+    }
 }
