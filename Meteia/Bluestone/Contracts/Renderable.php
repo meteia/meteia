80d5b1577a251ba14f0d429ba5726135e4aab978
Jason R. McNeil <jason@mcneil.dev>
2022-09-09 21:29:48 -0700

    bluestone: deprecate Renderable in favor of Stringable

--- Meteia/Bluestone/Contracts/Renderable.php
+++ Meteia/Bluestone/Contracts/Renderable.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace Meteia\Bluestone\Contracts;
 
-interface Renderable
+/**
+ * @deprecated Use \Stringable instead
+ */
+interface Renderable extends \Stringable
 {
     public function __toString();
 }
