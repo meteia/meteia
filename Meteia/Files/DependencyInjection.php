84133bf61c82046c62890b96b1e9c1c3b55321fa
Jason R. McNeil <jason@mcneil.dev>
2022-09-19 19:28:49 -0700

    files: storage backend from env

--- Meteia/Files/DependencyInjection.php
+++ Meteia/Files/DependencyInjection.php
@@ -3,13 +3,21 @@
 declare(strict_types=1);
 
 use Meteia\Configuration\Configuration;
+use Meteia\DependencyInjection\Container;
 use Meteia\Files\ContentAddressableStorageSecretKey;
 use Meteia\Files\Contracts\Storage;
 use Meteia\Files\LocalStorage;
+use Meteia\ObjectStorage\ObjectStorage;
 
 return [
-    Storage::class => LocalStorage::class,
+    Storage::class => function (Container $container, Configuration $configuration): Storage {
+        $backend = $configuration->string('METEIA_FILES_BACKEND', 'local');
+        return $container->get(match ($backend) {
+            'object' => ObjectStorage::class,
+            default => LocalStorage::class,
+        });
+    },
     ContentAddressableStorageSecretKey::class => function (Configuration $configuration) {
-        return ContentAddressableStorageSecretKey::fromToken($configuration->string('CONTENT_ADDRESSABLE_STORAGE_SECRET_KEY', 'invalid'));
+        return ContentAddressableStorageSecretKey::fromToken($configuration->string('METEIA_FILES_CONTENT_ADDRESSABLE_STORAGE_SECRET_KEY', 'invalid'));
     },
 ];
