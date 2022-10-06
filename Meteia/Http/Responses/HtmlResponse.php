286f6743ac356789dbd15bc683a45c847b52616e
Jason R. McNeil <jason@mcneil.dev>
2022-10-05 20:28:59 -0700

    http: Accept stringable|string

--- Meteia/Http/Responses/HtmlResponse.php
+++ Meteia/Http/Responses/HtmlResponse.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Meteia\Http\Responses;
 
-use Meteia\Bluestone\Contracts\Renderable;
+use Stringable;
 
 class HtmlResponse extends \Laminas\Diactoros\Response\HtmlResponse
 {
-    public function __construct(Renderable $renderable, $status = 200, array $headers = [])
+    public function __construct(Stringable|string $renderable, $status = 200, array $headers = [])
     {
         parent::__construct((string) $renderable, $status, $headers);
     }
