b2d1488ae0ac390aca677c1f79121ef97e5216d7
Jason R. McNeil <jason@mcneil.dev>
2022-12-04 01:36:16 -0700

    php-cs-fixer: update rules for 8.1

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -16,13 +16,17 @@ return $config
     ->setRiskyAllowed(true)
     ->setRules([
         '@Symfony' => true,
+        '@PHP80Migration:risky' => true,
+        '@PHP81Migration' => true,
         'ordered_imports' => ['sort_algorithm' => 'alpha', 'imports_order' => ['const', 'class', 'function']],
         'method_argument_space' => ['on_multiline' => 'ensure_fully_multiline'],
         'yoda_style' => false,
+        'global_namespace_import' => true,
         'trailing_comma_in_multiline' => ['elements' => ['arrays', 'arguments', 'parameters']],
         'concat_space' => ['spacing' => 'one'],
         'phpdoc_summary' => false,
         'increment_style' => false,
+        'echo_tag_syntax' => ['format' => 'short', 'shorten_simple_statements_only' => false],
 
         // green-field
         'strict_comparison' => true,
