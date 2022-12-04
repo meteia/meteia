c8021664b688b499e73856745ce8a6921ac8aeb0
Jason R. McNeil <jason@mcneil.dev>
2022-12-04 13:41:37 -0700

    html: add attrs for Link

--- Meteia/Html/Elements/Link.php
+++ Meteia/Html/Elements/Link.php
@@ -5,16 +5,19 @@ declare(strict_types=1);
 namespace Meteia\Html\Elements;
 
 use Meteia\Html\Element;
+use Stringable;
 
-class Link
+class Link implements Stringable
 {
     use Element;
 
     public function __construct(
         public string $rel,
         public string $href,
-        public ?string $integrity,
-        public ?string $crossorigin,
+        public ?string $integrity = null,
+        public ?string $crossorigin = null,
+        public ?string $sizes = null,
+        public ?string $type = null,
     ) {
     }
 }
