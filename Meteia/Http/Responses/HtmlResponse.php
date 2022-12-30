694ec5d44fcf8348b823c79a409d8cc8fb2014b0
Jason R. McNeil <jason@mcneil.dev>
2022-12-29 23:37:42 -0700

    http: allow null HtmlResponse

--- Meteia/Http/Responses/HtmlResponse.php
+++ Meteia/Http/Responses/HtmlResponse.php
@@ -8,7 +8,7 @@ use Stringable;
 
 class HtmlResponse extends \Laminas\Diactoros\Response\HtmlResponse
 {
-    public function __construct(Stringable|string $renderable, $status = 200, array $headers = [])
+    public function __construct(Stringable|string|null $renderable = null, $status = 200, array $headers = [])
     {
         parent::__construct((string) $renderable, $status, $headers);
     }
