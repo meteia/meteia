91d309d011688eab288959e1f73fbb607a74fe51
Jason R. McNeil <jason@mcneil.dev>
2023-01-21 19:16:12 -0700

    http: Log more of the request and the response

--- Meteia/Http/Middleware/SimpleLog.php
+++ Meteia/Http/Middleware/SimpleLog.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Meteia\Http\Middleware;
 
 use Meteia\Application\RepositoryPath;
+use Meteia\Http\RequestBody;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
@@ -13,16 +14,25 @@ use Psr\Http\Server\RequestHandlerInterface;
 class SimpleLog implements MiddlewareInterface
 {
     public function __construct(
-        private RepositoryPath $repositoryPath,
+        private readonly RequestBody $requestBody,
+        private readonly RepositoryPath $repositoryPath,
     ) {
     }
 
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
         $logFile = $this->repositoryPath->join('http.log');
-        $line = sprintf('%s %s', $request->getMethod(), $request->getUri()) . PHP_EOL;
+        $line = PHP_EOL . sprintf('--> %s %s', $request->getMethod(), $request->getUri()) . PHP_EOL;
+        if (strlen($this->requestBody->content())) {
+            $line .= "  " . $this->requestBody->content() . PHP_EOL;
+        }
         file_put_contents((string) $logFile, $line, FILE_APPEND | LOCK_EX);
 
-        return $handler->handle($request);
+        $response = $handler->handle($request);
+
+        $line = sprintf('  <-- %s', $response->getStatusCode()) . PHP_EOL;
+        file_put_contents((string) $logFile, $line, FILE_APPEND | LOCK_EX);
+
+        return $response;
     }
 }
