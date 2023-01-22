5a279ea9d5fde1300e372aadf498d728f3669fbb
Jason R. McNeil <jason@mcneil.dev>
2023-01-21 19:17:37 -0700

    dulce: Fix debug JSON response header check

--- Meteia/Dulce/Endpoints/DeveloperErrorEndpoint.php
+++ Meteia/Dulce/Endpoints/DeveloperErrorEndpoint.php
@@ -22,7 +22,7 @@ class DeveloperErrorEndpoint implements ErrorEndpoint
 
     public function response(Throwable $throwable, ServerRequestInterface $request): ResponseInterface
     {
-        if (str_contains($request->getHeaderLine('Content-Type'), 'application/json')) {
+        if (str_contains($request->getHeaderLine('Accept'), 'application/json')) {
             return new JsonResponse([
                 'message' => $throwable->getMessage(),
                 'stackTrace' => array_values(array_map(fn ($frame) => implode(':', [$frame->file, $frame->line]), $this->stackTrace->for($throwable)->stackFrames())),
