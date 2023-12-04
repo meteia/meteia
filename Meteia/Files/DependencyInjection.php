131281b9bc8aa2cb1d9c41eb7fa031cef23498b8
Jason R. McNeil <jason@mcneil.dev>
2023-12-03 23:01:53 -0700

    files: add PublicUri for ObjectStorage

--- Meteia/Files/DependencyInjection.php
+++ Meteia/Files/DependencyInjection.php
@@ -7,6 +7,7 @@ use Meteia\Files\Configuration\AccessKey;
 use Meteia\Files\Configuration\BucketName;
 use Meteia\Files\Configuration\ContentAddressableStorageSecretKey;
 use Meteia\Files\Configuration\Endpoint;
+use Meteia\Files\Configuration\PublicUri;
 use Meteia\Files\Configuration\Region;
 use Meteia\Files\Configuration\SecretKey;
 use Meteia\Files\Contracts\Storage;
@@ -18,6 +19,7 @@ return [
     SecretKey::class => fn (Configuration $configuration): SecretKey => new SecretKey($configuration->string('METEIA_FILES_OBJECT_STORAGE_SECRET_KEY', '')),
     BucketName::class => fn (Configuration $configuration): BucketName => new BucketName($configuration->string('METEIA_FILES_OBJECT_STORAGE_BUCKET', '')),
     Endpoint::class => fn (Configuration $configuration): Endpoint => new Endpoint($configuration->string('METEIA_FILES_OBJECT_STORAGE_ENDPOINT', '')),
+    PublicUri::class => fn (Configuration $configuration): PublicUri => new PublicUri($configuration->string('METEIA_FILES_OBJECT_STORAGE_PUBLIC_URI', $configuration->string('METEIA_FILES_OBJECT_STORAGE_ENDPOINT', ''))),
     Region::class => fn (Configuration $configuration): Region => new Region($configuration->string('METEIA_FILES_OBJECT_STORAGE_REGION', '')),
     ContentAddressableStorageSecretKey::class => fn (Configuration $configuration) => ContentAddressableStorageSecretKey::fromToken($configuration->string('METEIA_FILES_CONTENT_ADDRESSABLE_STORAGE_SECRET_KEY', 'invalid')),
     Storage::class => fn (Configuration $configuration) => match ($configuration->string('METEIA_FILES_STORAGE', 'local')) {
