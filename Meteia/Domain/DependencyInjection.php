a43b1c2d36664761bbe5117a01128da814a661ab
Jason R. McNeil <jason@mcneil.dev>
2023-12-09 02:51:43 -0700

    domain: Set default DomainCommandsExchangeName

--- Meteia/Domain/DependencyInjection.php
+++ Meteia/Domain/DependencyInjection.php
@@ -2,9 +2,12 @@
 
 declare(strict_types=1);
 
+use Meteia\Configuration\Configuration;
+use Meteia\Domain\Configuration\DomainCommandsExchangeName;
 use Meteia\Domain\Contracts\UnitOfWork;
 use Meteia\Domain\ImmediateUnitOfWork;
 
 return [
     UnitOfWork::class => ImmediateUnitOfWork::class,
+    DomainCommandsExchangeName::class => fn (Configuration $configuration): DomainCommandsExchangeName => new DomainCommandsExchangeName($configuration->string('METEIA_DOMAIN_COMMANDS_EXCHANGE_NAME', 'commands')),
 ];
