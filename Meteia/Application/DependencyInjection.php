829095bbd7c6d7e6f95b4952be2a74eba9e183b7
Jason R. McNeil <jason@mcneil.dev>
2023-12-09 02:49:50 -0700

    application: Set default RepositoryPath

--- Meteia/Application/DependencyInjection.php
+++ Meteia/Application/DependencyInjection.php
@@ -6,10 +6,12 @@ use Meteia\Application\ApplicationPath;
 use Meteia\Application\ApplicationPublicDir;
 use Meteia\Application\ApplicationResources;
 use Meteia\Application\ApplicationResourcesBaseUri;
+use Meteia\Application\RepositoryPath;
 use Meteia\Configuration\Configuration;
 
 return [
     ApplicationResourcesBaseUri::class => fn (Configuration $configuration) => new ApplicationResourcesBaseUri($configuration->string('RESOURCES_BASE_URI', '')),
     ApplicationResources::class => fn (ApplicationResourcesBaseUri $applicationResourcesBaseUri, ApplicationPublicDir $publicDir): ApplicationResources => new ApplicationResources($applicationResourcesBaseUri, $publicDir, $publicDir->join('dist/manifest.json')),
     ApplicationPublicDir::class => fn (ApplicationPath $applicationPath) => new ApplicationPublicDir($applicationPath->join('public')),
+    RepositoryPath::class => fn (ApplicationPath $applicationPath) => new RepositoryPath($applicationPath),
 ];
