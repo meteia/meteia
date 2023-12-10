eac918ea5c78c0906aaa38116212b7a9a6b4d5fb
Jason R. McNeil <jason@mcneil.dev>
2023-12-10 12:28:45 -0700

    cleanup: remove dead/moved code

--- Meteia/Domain/DependencyInjection.php
+++ Meteia/Domain/DependencyInjection.php
@@ -2,12 +2,9 @@
 
 declare(strict_types=1);
 
-use Meteia\Configuration\Configuration;
-use Meteia\Domain\Configuration\DomainCommandsExchangeName;
 use Meteia\Domain\Contracts\UnitOfWork;
 use Meteia\Domain\ImmediateUnitOfWork;
 
 return [
     UnitOfWork::class => ImmediateUnitOfWork::class,
-    DomainCommandsExchangeName::class => fn (Configuration $configuration): DomainCommandsExchangeName => new DomainCommandsExchangeName($configuration->string('METEIA_DOMAIN_COMMANDS_EXCHANGE_NAME', 'commands')),
 ];
