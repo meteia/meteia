06e8f8b74e5f98330831e1ea0f159b0d1226e890
Jason R. McNeil <jason@mcneil.dev>
2024-02-11 01:26:04 -0700

    nix: remove dead code

--- nix/devShells/default.nix
+++ nix/devShells/default.nix
@@ -6,34 +6,7 @@
   chips,
   ...
 }:
-with lib; let
-  vscodeSettings = pkgs.writeText "settings.json" (builtins.toJSON {
-    "php.validate.executablePath" = "${pkgs.php}/bin/php";
-    "files.exclude" = {
-      "**/.chips" = true;
-      "**/.task" = true;
-    };
-  });
-
-  taskConfig = {
-    tasks = {
-      composer = {
-        cmds = ["${pkgs.php.packages.composer}/bin/composer install"];
-        generates = ["vendor/composer/installed.json" "vendor/autoload.php"];
-        label = "Install Composer Dependencies";
-        sources = ["composer.json" "composer.lock"];
-      };
-      node_modules = {
-        cmds = ["${pkgs.nodePackages.pnpm}/bin/pnpm install"];
-        generates = ["node_modules/.modules.yaml"];
-        label = "Install Node.JS Dependencies";
-        sources = ["package.json" "pnpm-lock.yaml"];
-      };
-    };
-    version = "3";
-  };
-  taskConfigFile = pkgs.writeText "Taskfile.yml" (builtins.toJSON taskConfig);
-in {
+with lib; {
   imports = [
   ];
   options = with types; {
@@ -44,11 +17,7 @@ in {
     {
       devShell = {
         contents = with pkgs; [];
-        environment = [
-        ];
-        shellHooks = ''
-          # ln -sf ${vscodeSettings} .vscode/settings.json
-        '';
+        environment = [];
       };
 
       programs.lefthook.enable = true;
