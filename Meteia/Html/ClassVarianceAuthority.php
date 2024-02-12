905b3d1c91f65c516111551f37c5099e3d772835
Jason R. McNeil <jason@mcneil.dev>
2024-02-12 00:25:38 -0700

    html: Use strings instead of arrays for classes in ClassVarianceAuthority

--- Meteia/Html/ClassVarianceAuthority.php
+++ Meteia/Html/ClassVarianceAuthority.php
@@ -6,19 +6,26 @@ namespace Meteia\Html;
 
 class ClassVarianceAuthority implements ClassName
 {
-    public function __construct(private readonly array $core = [], private readonly array $variants = [])
+    public function __construct(private readonly string $class = '', private readonly array $variants = [])
     {
     }
 
     public function use(array $props): string
     {
         $activeVariants = array_map(
-            fn ($variantName, $variant) => implode(' ', $this->variants[$variantName][$variant] ?? []),
+            fn ($variantName, $variant) => $this->variants[$variantName][$variant] ?? '',
             array_keys($props),
             $props,
         );
 
-        return implode(' ', [...$this->core, ...$activeVariants]);
+        $coreClassNames = explode(' ', $this->class);
+        $activeVariantClassNames = explode(' ', implode(' ', $activeVariants));
+        $allClassNames = [...$coreClassNames, ...$activeVariantClassNames];
+        $allClassNames = array_map('trim', $allClassNames);
+        $allClassNames = array_filter($allClassNames, static fn ($className) => $className !== '');
+        $uniqueClassNames = array_unique($allClassNames);
+
+        return implode(' ', $uniqueClassNames);
     }
 
     public function attribute(array $props): ClassAttribute
