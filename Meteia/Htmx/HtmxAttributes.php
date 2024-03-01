d561bb69c899ddf3fcd4399def67aee569dc8e7f
Jason R. McNeil <jason@mcneil.dev>
2024-03-01 01:54:16 -0700

    htmx: make class readonly, split toArray out from __toString

--- Meteia/Htmx/HtmxAttributes.php
+++ Meteia/Htmx/HtmxAttributes.php
@@ -6,59 +6,67 @@ namespace Meteia\Htmx;
 
 use Meteia\Library\StringCase;
 
-class HtmxAttributes implements \Stringable
+use function Meteia\Polyfills\array_map_assoc;
+
+readonly class HtmxAttributes implements \Stringable
 {
-    /** @noinspection PhpPropertyOnlyWrittenInspection */
     public function __construct(
-        private readonly ?bool $boost = false,
-        private readonly ?string $confirm = null,
-        private readonly ?string $delete = null,
-        private readonly ?string $disable = null,
-        private readonly ?string $disinherit = null,
-        private readonly ?string $encoding = null,
-        private readonly ?string $ext = null,
-        private readonly ?string $get = null,
-        private readonly ?string $headers = null,
-        private readonly ?string $history = null,
-        private readonly ?string $historyElt = null,
-        private readonly ?string $include = null,
-        private readonly ?string $indicator = null,
-        private readonly ?string $params = null,
-        private readonly ?string $patch = null,
-        private readonly ?string $post = null,
-        private readonly ?string $preserve = null,
-        private readonly ?string $prompt = null,
-        private readonly ?string $pushUrl = null,
-        private readonly ?string $put = null,
-        private readonly ?string $replaceUrl = null,
-        private readonly ?string $request = null,
-        private readonly ?string $select = null,
-        private readonly ?string $selectOob = null,
-        private readonly ?string $swap = null,
-        private readonly ?string $swapOob = null,
-        private readonly ?string $sync = null,
-        private readonly ?string $target = null,
-        private readonly ?string $trigger = null,
-        private readonly ?string $validate = null,
-        private readonly ?string $vals = null,
+        private ?bool $boost = false,
+        private ?string $confirm = null,
+        private ?string $delete = null,
+        private ?string $disable = null,
+        private ?string $disinherit = null,
+        private ?string $encoding = null,
+        private ?string $ext = null,
+        private ?string $get = null,
+        private ?string $headers = null,
+        private ?string $history = null,
+        private ?string $historyElt = null,
+        private ?string $include = null,
+        private ?string $indicator = null,
+        private ?string $params = null,
+        private ?string $patch = null,
+        private ?string $post = null,
+        private ?string $preserve = null,
+        private ?string $prompt = null,
+        private ?string $pushUrl = null,
+        private ?string $put = null,
+        private ?string $replaceUrl = null,
+        private ?string $request = null,
+        private ?string $select = null,
+        private ?string $selectOob = null,
+        private ?string $swap = null,
+        private ?string $swapOob = null,
+        private ?string $sync = null,
+        private ?string $target = null,
+        private ?string $trigger = null,
+        private ?string $validate = null,
+        private ?string $vals = null,
     ) {
     }
 
     public function __toString(): string
     {
-        $attrs = array_filter(get_object_vars($this), static fn ($val) => !empty($val));
+        $attrs = $this->toArray();
         $attrs = array_map(
             static function ($k, $v) {
                 if (\is_bool($v) && $v) {
                     return $k;
                 }
 
-                return sprintf('hx-%s="%s"', StringCase::kebab($k), $v);
+                return sprintf('%s="%s"', $k, $v);
             },
             array_keys($attrs),
             $attrs,
         );
 
         return implode(' ', $attrs);
+    }
+
+    public function toArray(): array
+    {
+        $attrs = array_filter(get_object_vars($this), static fn ($val) => !empty($val));
+
+        return array_map_assoc(static fn ($k, $v) => ['hx-' . StringCase::kebab($k) => $v], $attrs);
     }
 }
