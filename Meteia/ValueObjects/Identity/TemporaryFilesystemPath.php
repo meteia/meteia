9e15beae546ce89f57537310210a786dbf6acf1d
Jason R. McNeil <jason@mcneil.dev>
2024-05-03 22:12:33 -0700

    ValueObjects: add TemporaryFilesystemPath::fromStream

--- Meteia/ValueObjects/Identity/TemporaryFilesystemPath.php
+++ Meteia/ValueObjects/Identity/TemporaryFilesystemPath.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Meteia\ValueObjects\Identity;
 
+use Psr\Http\Message\StreamInterface;
+
 class TemporaryFilesystemPath extends FilesystemPath
 {
     public function __destruct()
@@ -19,5 +21,17 @@ class TemporaryFilesystemPath extends FilesystemPath
         file_put_contents($tempPath, $data);
 
         return new self($tempPath);
+    }
+
+    public static function fromStream(StreamInterface $stream): self
+    {
+        $resource = new Resource($stream->detach());
+
+        $tempPath = tempnam(sys_get_temp_dir(), 'meteia');
+        $self = new self($tempPath);
+
+        $resource->writeStream($self);
+
+        return $self;
     }
 }
