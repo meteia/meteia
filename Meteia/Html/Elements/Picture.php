fd690dc10c40a6e20d01d4a55fbcf52fb2ab2ad4
Jason R. McNeil <jason@mcneil.dev>
2024-08-10 11:05:49 -0700

    html: make Picture props public

--- Meteia/Html/Elements/Picture.php
+++ Meteia/Html/Elements/Picture.php
@@ -4,18 +4,18 @@ declare(strict_types=1);
 
 namespace Meteia\Html\Elements;
 
-class Picture
+readonly class Picture
 {
     /**
      * @param array<int, Source> $sources
      */
-    public function __construct(protected Img $img, protected array $sources)
+    public function __construct(public Img $img, public array $sources)
     {
     }
 
     public function __toString(): string
     {
-        return sprintf(
+        return \sprintf(
             '<%s>%s</%s>' . PHP_EOL,
             'picture',
             implode("\n", array_map('strval', [...$this->sources, $this->img])),
