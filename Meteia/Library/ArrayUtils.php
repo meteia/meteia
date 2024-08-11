00df5f7148658c9abb61222ba171bc1322decf47
Jason R. McNeil <jason@mcneil.dev>
2024-08-11 15:25:08 -0700

    library: add array_first

--- Meteia/Library/ArrayUtils.php
+++ Meteia/Library/ArrayUtils.php
@@ -3,3 +3,24 @@
 declare(strict_types=1);
 
 namespace Meteia\Library\ArrayUtils;
+
+function array_first(array $array, ?callable $callback = null, $default = null)
+{
+    if ($callback === null) {
+        if (empty($array)) {
+            return $default;
+        }
+
+        foreach ($array as $item) {
+            return $item;
+        }
+    }
+
+    foreach ($array as $key => $value) {
+        if ($callback($value, $key)) {
+            return $value;
+        }
+    }
+
+    return $default;
+}
