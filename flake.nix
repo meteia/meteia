37ba010218a201d6b12396f204962eb6e30d9b29
Jason R. McNeil <jason@mcneil.dev>
2025-08-31 14:05:39 -0700

    cleanup old tooling

--- flake.nix
+++ flake.nix
@@ -6,31 +6,9 @@
     chips.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    { chips, ... }:
+  outputs = {chips, ...}:
     chips.lib.use {
       devShellsDir = ./nix/devShells;
-      overlays = [
-        (self: super: {
-          php = self.php83.buildEnv {
-            extensions =
-              { enabled, all, ... }:
-              with all;
-              enabled
-              ++ [
-                apcu
-                event
-                imagick
-                igbinary
-              ];
-            extraConfig = ''
-              output_buffering = 4096
-              post_max_size = 100M
-              upload_max_filesize = 100M
-              variables_order = EGPCS
-            '';
-          };
-        })
-      ];
+      overlays = [chips.overlays.default];
     };
 }
--- nix/devShells/default.nix
+++ nix/devShells/default.nix
@@ -23,6 +23,12 @@ with lib; {
       programs.nodejs.enable = true;
       programs.php = {
         pkg = pkgs.php84;
+        extraConfig = ''
+          output_buffering = 4096
+          post_max_size = 100M
+          upload_max_filesize = 100M
+          variables_order = EGPCS
+        '';
         extensions = {
           enabled,
           all,
@@ -32,6 +38,8 @@ with lib; {
             enabled
             ++ [
               apcu
+              event
+              imagick
               igbinary
             ];
         enable = true;
