7c7be34005f9c284b089463278abeb26df7cffbb
Jason R. McNeil <jason@mcneil.dev>
2025-08-30 22:19:36 -0700

    nix: update flakes

--- flake.nix
+++ flake.nix
@@ -1,35 +1,28 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
 
     chips.url = "github:jasonrm/nix-chips";
     chips.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    chips,
-    ...
-  }:
+  outputs =
+    { chips, ... }:
     chips.lib.use {
       devShellsDir = ./nix/devShells;
       overlays = [
         (self: super: {
           php = self.php83.buildEnv {
-            extensions = {
-              enabled,
-              all,
-              ...
-            }:
+            extensions =
+              { enabled, all, ... }:
               with all;
-                enabled
-                ++ [
-                  apcu
-                  event
-                  imagick
-                  igbinary
-                ];
+              enabled
+              ++ [
+                apcu
+                event
+                imagick
+                igbinary
+              ];
             extraConfig = ''
               output_buffering = 4096
               post_max_size = 100M
