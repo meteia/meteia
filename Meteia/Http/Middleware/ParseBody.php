43f7fc76b8eb6f87a27293b19c4d2c7ef306da3a
Jason R. McNeil <jason@mcneil.dev>
2025-09-05 19:29:14 -0700

    http: use array_any for json body detection

--- Meteia/Http/Middleware/ParseBody.php
+++ Meteia/Http/Middleware/ParseBody.php
@@ -14,15 +14,22 @@ class ParseBody implements MiddlewareInterface
     #[\Override]
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $ct = implode('', $request->getHeader('Content-Type'));
-        if (str_contains($ct, 'application/json')) {
+        $isJsonBody = array_any($request->getHeader('Content-Type'), static fn($ct) => str_contains(
+            $ct,
+            'application/json',
+        ));
+        if ($isJsonBody) {
             $body = $request->getBody();
             if ($body->isSeekable()) {
                 $body->rewind();
             }
             $contents = $body->getContents();
-            $json = json_decode($contents, true, 256, JSON_THROW_ON_ERROR);
-            $request = $request->withParsedBody($json);
+            try {
+                $json = json_decode($contents, true, 256, JSON_THROW_ON_ERROR);
+                $request = $request->withParsedBody($json);
+            } catch (\JsonException) {
+                // Ignore JSON parse errors
+            }
         }
 
         return $handler->handle($request);
